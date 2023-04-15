94f1087416457585abd82c3f559dc1db9648d29c
AnatolSalau <salovanatoly5@mail.ru>
2023-04-15 21:09:59 +0300

    init commit

---
+++ src/main/java/com/example/cors_react_ui/CorsReactUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cors_react_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsReactUiApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(CorsReactUiApplication.class, args);
+      }
+
+}
---
+++ src/test/java/com/example/cors_react_ui/CorsReactUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cors_react_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsReactUiApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
